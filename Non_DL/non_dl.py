2cc197562022cf06581c620f7476906126c32984
Siddarth Vinnakota <77826041+sidvin101@users.noreply.github.com>
2025-03-20 20:28:19 -0400

    Update non_dl.py
    I made some quick changes. When I switched the models, I forgot to delete original model code. This fix updates that.

--- Non_DL/non_dl.py
+++ Non_DL/non_dl.py
@@ -5,8 +5,6 @@ import json
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import MultinomialNB
-#Import randomforest classifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
@@ -31,34 +29,33 @@ df_tot = pd.concat([df_gpt2, df_gpt3, df_human, df_llama])
 #Removing a column
 df_tot = df_tot[["text", "label"]]
 
-#Perform the train test split
+#Performing the train test split
 X_train, X_test, y_train, y_test = train_test_split(df_tot['text'], df_tot['label'], test_size=0.2, random_state=0)
 
-#Create a count vectorizer and transform
+#Creating a count vectorizer and transform
 count_vect = CountVectorizer()
 X_train_counts = count_vect.fit_transform(X_train)
-#Create a tfidf transformer and transform
+#Creating a tfidf transformer and transform
 tfidf_transformer = TfidfTransformer()
 X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
 
-#Create a Multinomial Naive Bayes classifier
-#clf = MultinomialNB().fit(X_train_tfidf, y_train)
+#Creating a RandomForestClassifier
 clf = RandomForestClassifier(n_estimators = 200, max_depth = 3, random_state = 0)
 clf.fit(X_train_tfidf, y_train)
 
-#Transform the test data
+#Transforming the test data
 X_test_counts = count_vect.transform(X_test)
 X_test_tfidf = tfidf_transformer.transform(X_test_counts)
 
-#Predict the labels
+#Predicting the labels
 y_preds = clf.predict(X_test_tfidf)
 
-#Print the confusion matrix
+#Printing the confusion matrix
 labels = ['gpt2', 'gpt3', 'human', 'llama']
 cm = confusion_matrix(y_test, y_preds)
 print(cm)
 
-#Print the overall metrics
+#Printing the overall metrics
 accuracy = accuracy_score(y_test, y_preds)
 precision = precision_score(y_test, y_preds, average='macro')
 recall = recall_score(y_test, y_preds, average='macro')
@@ -71,7 +68,7 @@ print("Recall: ", round(float(recall), 4))
 print("F1 Score: ", round(float(f1), 4))
 print()
 
-#Print the metrics for each class
+#Printing the metrics for each class
 accuracies_per_class = np.array([np.sum((y_test == i) & (y_preds == i)) / np.sum(y_test == i) for i in np.unique(y_test)])
 precision_per_class = precision_score(y_test, y_preds, average=None)
 recall_per_class = recall_score(y_test, y_preds, average=None)
@@ -85,7 +82,7 @@ for i, label in enumerate(sorted(set(y_test))):
     print("F1 Score: ", f1_per_class[i].round(4))
     print()
 
-#Save the model and the vectorizers
+#Saving the model and the vectorizers
 pickle.dump(count_vect, open('count_vect.pkl', 'wb'))
 pickle.dump(tfidf_transformer, open('tfidf_transformer.pkl', 'wb'))
 pickle.dump(clf, open('clf.pkl', 'wb'))
